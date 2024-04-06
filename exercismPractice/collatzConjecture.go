a8205fc5fe97ba643f8b31d1981299268ca3cde3
kino <kino@pop-os.localdomain>
2024-04-06 09:47:25 +0200

    collatz

---
+++ exercismPractice/collatzConjecture.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+)
+
+func CollatzConjecture(n int) (int, error) {
+	c := 0
+	if n <= 0 {
+		// err := fmt.Errorf("Integers must be greater than zero")
+		// return 0, err
+        return 0, errors.New("input must be a positive integer")
+    }
+	for n > 1 {
+		if n%2 == 0 {
+			n /= 2
+		} else {
+			n = n*3 + 1
+		}
+		c++
+	}
+	return c, nil
+}
+
+func main() {
+	fmt.Println(CollatzConjecture(12))
+}
+
+// 
